9285688af2f1a01817a229bebde6ff228b0f45ce
mossv2 <mossv2@github.com>
2024-12-11 21:17:33 +0800

    fix

--- build_hub.sh
+++ build_hub.sh
@@ -2,9 +2,7 @@
 
 version=$1
 
-docker build -t dimo-hub:$version -f Dockerfile .
-docker tag dimo-hub:$version dimo-hub:latest
-docker tag dimo-hub:$version mossv2/dimo:hub-$version
-docker tag dimo-hub:$version mossv2/dimo:hub-latest
+docker build -t mossv2/dimo:hub-$version -f Dockerfile .
+docker tag mossv2/dimo:hub-$version mossv2/dimo:hub-latest
 docker push mossv2/dimo:hub-$version
 docker push mossv2/dimo:hub-latest
